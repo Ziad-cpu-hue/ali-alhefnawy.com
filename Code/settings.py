5f8a9d1cc85f0607761e692d2a131ecaaf28dc20
Ziad-cpu-hue <code37265@gmail.com>
2025-07-06 16:48:24 +0300

    Update settings.py

--- Code/settings.py
+++ Code/settings.py
@@ -52,7 +52,6 @@ MESSAGE_TAGS = {
 # Application definition
 
 INSTALLED_APPS = [
-    #'jazzmin',  # ← لازم يكون في البداية
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
