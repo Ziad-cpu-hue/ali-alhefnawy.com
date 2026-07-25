45f0f6179dab1c157e138e1a5e4c65992a2691ff
Ziad-cpu-hue <code37265@gmail.com>
2026-07-25 21:15:20 +0300

    Delete content directory

--- content/templatetags/custom_filters.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def get_dict_value(dictionary, key):
-    return dictionary.get(key, "لم يتم الإجابة") if isinstance(dictionary, dict) else "لم يتم الإجابة"
